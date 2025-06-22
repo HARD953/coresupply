d83d6d333dbad858e6009d57041d68638613e9ec
HARD953 <issa97403@gmail.com>
2025-06-22 22:54:16 +0000

    first commit4

--- lanfiatech_backend/settings.py
+++ lanfiatech_backend/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-6-ou#njn0ns&#y^obrrfkt*9+fw!kyea=c4cypv#vfk+#bv844
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
